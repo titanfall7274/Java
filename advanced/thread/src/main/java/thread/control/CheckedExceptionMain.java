71e371ed43d41eec64ee5feb5af5fc4c8d12f5ef
titanfall <qkrwogh7366@gmail.com>
2026-09-04 18:26:42 +0900

    feat: 스레드 제어와 생명 주기

---
+++ advanced/thread/src/main/java/thread/control/CheckedExceptionMain.java
@@ -0,0 +1,27 @@
+package thread.control;
+
+import static util.ThreadUtils.sleep;
+
+public class CheckedExceptionMain {
+
+    public static void main(String[] args) throws Exception {
+        throw new Exception();
+    }
+
+    static class CheckedRunnable implements Runnable {
+        @Override
+        public void run() /*throws Exception*/ {
+            // throw nwe Exception; // 예외 던지기가 불가능합니다.
+
+
+            // ThreadUtils.sleep(millis);
+            sleep(1000);
+
+//            try {
+//                throw new Exception();
+//            } catch (Exception e) {
+//                throw new RuntimeException(e);
+//            }
+        }
+    }
+}
